d48a4370ba8498a31ec050b2bbd1428fab60fecf
BRIANOSIEKU <brianosieku@gmail.com>
2025-10-07 15:13:24 +0300

    feat(api): implement CRUD actions for Api::V1::FactsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get "facts/index"
+      get "facts/show"
+      get "facts/create"
+      get "facts/update"
+      get "facts/destroy"
+      get "users/index"
+      get "users/show"
+      get "users/create"
+      get "users/update"
+      get "users/destroy"
+    end
+  end
   get "home/index"
   root "home#index"
 end
