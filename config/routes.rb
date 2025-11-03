348d55cac4432976544d0ab2c5d101e1fce9661b
root <root@DESKTOP-K9EV2RL.localdomain>
2025-11-03 11:05:53 +0300

    Fix routes.rb structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
-namespace :api do
-  namespace :v1 do
-    resources :users
-    resources :facts do
-      member do
-        post 'like'
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :facts do
+        member do
+          post 'like'
+        end
       end
     end
   end
