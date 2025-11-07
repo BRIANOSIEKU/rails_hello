19dba08e3eb0a4fb63795598fe95608e34a51d35
root <root@DESKTOP-K9EV2RL.localdomain>
2025-11-07 12:00:22 +0300

    Fixed Rubocup error

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class FactsController < ApplicationController
-      before_action :set_fact, only: [:show, :update, :destroy, :like]
+      before_action :set_fact, only: [ :show, :update, :destroy, :like ]
 
       # your existing actions here...
       def index
